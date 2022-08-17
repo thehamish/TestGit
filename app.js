53e72843f7bb03cb3329e2798b476a6c65b77f23
HamishLP <heppet135@gmail.com>
2022-08-17 16:10:41 +0100

    script working with page now:)

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// idea to make descriptions of all my projects
+// will show the various projects and demonstrate
+// thought process, execution, brief
+
+let title, description, format;
+let message = document.querySelector('.FirstCommit');
+// title = "Hamish's First Commit";
+
+console.log(title,description,format,message);
+message.textContent = "Hello";
+document.write('yes');
+
+
+
