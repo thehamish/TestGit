64643b96be27ef8b78a29d48f9e5e07f30f2990e
HamishLP <heppet135@gmail.com>
2022-08-19 14:43:18 +0100

    added project text got text to wrap

--- app.js
+++ app.js
@@ -4,13 +4,15 @@
 // will show the various projects and demonstrate
 // thought process, execution, brief
 
-let title, description, format;
-let message = document.querySelector('.FirstCommit');
+let title, description, format, message;
+message = document.querySelector('.FirstCommit');
+description = document.querySelector('.Project');
 title = document.querySelector('.title');
 // title = "Hamish's First Commit";
 
 console.log(title,description,format,message);
 message.textContent = "HLP•22";
+description.textContent = "Lorem ipsum here is my text which isn't that long. Lorem ipsum here is my text which isn't that long. Lorem ipsum here is my text which isn't that long.";
 title.textContent = "1";
 // document.write('yes');
 
